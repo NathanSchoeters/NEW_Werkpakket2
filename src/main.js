074315cf296c2a975c8cf86ed5cbb0192c70af6a
Nathan Schoeters <nathanschoeters@Nathans-MacBook-Air.local>
2023-12-12 16:30:05 +0100

    Alles wat ik al had

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+import './scss/main.scss'
+
+
+
+import { createApp } from 'vue';
+
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+
+const pinia = createPinia();
+
+App.use(pinia);
+
+createApp(App).mount('#app');
---
+++ src/views/LoginView.vue
@@ -0,0 +1,6 @@
+<script>
+export default {
+    
+}
+</script>
+<template><h1>Log in</h1></template>
